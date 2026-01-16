c6a862843755eb7ea1dfadc1d1684eec038fd197
CARSON_WONG <wong.carson@northeastern.edu>
2026-01-16 15:36:43 -0800

    DONE 3

--- tests/test_q1.py
+++ tests/test_q1.py
@@ -8,19 +8,20 @@ from src.q1 import is_palindrome
 
 class testispalindrome(unittest.TestCase):
 
-    def test_simple_palindrome(self):
+
+    def test_simple_palindrome(self) -> None:
         self.assertTrue(is_palindrome("racecar"))
 
-    def test_simple_not_palindrome(self):
-        self.assertFalse(is_palindrome("hello"))
+    def test_simple_not_palindrome(self) -> None:
+        self.assertFalse(is_palindrome("This is not a palindrome"))
 
-    def test_case_insensitive(self):
+    def test_case_insensitive(self) -> None:
         self.assertTrue(is_palindrome("RaceCar"))
 
-    def test_with_spaces(self):
+    def test_with_spaces(self) -> None:
         self.assertTrue(is_palindrome("sit on a potato pan otis"))
 
-    def test_empty_string(self):
+    def test_empty_string(self) -> None:
         self.assertTrue(is_palindrome(""))
 
 
